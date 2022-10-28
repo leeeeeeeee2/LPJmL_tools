05196451b102ca4d8ba6d9514d83de228a8d64bf
Rutger Dankers <rutger.dankers@wur.nl>
2022-10-28 18:56:08 +0200

    changes to concatenation script, now looping over all files in one go

--- analysis/bangladesh/isimip3b/concat_lpjml_output.sh
+++ analysis/bangladesh/isimip3b/concat_lpjml_output.sh
@@ -12,14 +12,24 @@ module load cdo/gcc/64/1.9.3
 #mswc1.nc
 #mswc2.nc
 #myfile="mwateramount.nc"
-myfile="pft_harvest.grid.bin"
+#myfile="pft_harvest.grid.bin"
 
-histdir="/lustre/scratch/WUR/ESG/danke010/LPJmL/output/bangladesh/limirr/igbmasked_mri-esm2-0_hist"
+allfiles="mdischarge.nc mirrig.nc mevap.nc mwateramount.nc pft_harvest.grid.bin"
+
+model='gfdl-esm4'  #'mpi-esm1-2-hr' #'ipsl-cm6a-lr' #"ukesm1-0-ll" #mri-esm2-0
+scen="ssp585"
 #allyears=($(seq 1951 10 2011))
-sspdir="/lustre/scratch/WUR/ESG/danke010/LPJmL/output/bangladesh/limirr/igbmasked_mri-esm2-0_ssp585"
 
-# merge files from hist run
-cdo mergetime ${histdir}/1951/${myfile} ${histdir}/1961/${myfile} ${histdir}/1971/${myfile} ${histdir}/1981/${myfile} ${histdir}/1991/${myfile} ${histdir}/2001/${myfile} ${histdir}/2011/${myfile} ${histdir}/${myfile}
+histdir="/lustre/scratch/WUR/ESG/danke010/LPJmL/output/bangladesh/limirr/igbmasked_${model}_hist"
+sspdir="/lustre/scratch/WUR/ESG/danke010/LPJmL/output/bangladesh/limirr/igbmasked_${model}_${scen}"
 
-# merge files from SSP run
-cdo mergetime ${sspdir}/2015/${myfile} ${sspdir}/2021/${myfile} ${sspdir}/2031/${myfile} ${sspdir}/2041/${myfile} ${sspdir}/2051/${myfile} ${sspdir}/2061/${myfile} ${sspdir}/2071/${myfile} ${sspdir}/2081/${myfile} ${sspdir}/2091/${myfile} ${sspdir}/${myfile}
+for myfile in ${allfiles}; do 
+  echo ${myfile}; 
+
+  # merge files from hist run
+  cdo mergetime ${histdir}/1951/${myfile} ${histdir}/1961/${myfile} ${histdir}/1971/${myfile} ${histdir}/1981/${myfile} ${histdir}/1991/${myfile} ${histdir}/2001/${myfile} ${histdir}/2011/${myfile} ${histdir}/${myfile}
+
+  # merge files from SSP run
+  cdo mergetime ${sspdir}/2015/${myfile} ${sspdir}/2021/${myfile} ${sspdir}/2031/${myfile} ${sspdir}/2041/${myfile} ${sspdir}/2051/${myfile} ${sspdir}/2061/${myfile} ${sspdir}/2071/${myfile} ${sspdir}/2081/${myfile} ${sspdir}/2091/${myfile} ${sspdir}/${myfile}  
+
+done
